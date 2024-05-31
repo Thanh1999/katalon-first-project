863e023d3f7aa4f336cb358f006f5e2487e557a4
Thanh1999 <t.max347056@gmail.com>
2024-05-31 21:03:29 +0700

    add new tc

---
+++ Test Suites/Lazada/Open Shoppee.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Open Shoppee</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e7ba5547-8ef1-45bf-8003-f95ccc964c30</testSuiteGuid>
+   <testCaseLink>
+      <guid>3eaf9a92-058d-4e85-b7d8-1a990094ec88</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Lazada/Br</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
